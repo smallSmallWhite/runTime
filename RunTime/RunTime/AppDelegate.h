3755c57c67c8630c880b68bd9528c777b1753b59
admin <1395258209@qq.com>
2017-01-10 15:28:08 +0800

    创建一个项目

---
+++ RunTime/RunTime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RunTime
+//
+//  Created by admin on 17/1/10.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
